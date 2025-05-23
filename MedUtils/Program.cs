4374fe3122ccc183c6ba2a44be9bca76189e7634
Rodolphe Bailly <rodolphe1970@gmail.com>
2025-05-23 14:40:16 +0200

    Code refactoring

--- MedUtils/Program.cs
+++ MedUtils/Program.cs
@@ -1,79 +1,24 @@
 using MARC;
+using MedUtils.Tools;
+using System.Xml.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello les savoirs");
-string accessKey = "85kxBW8Lm2vQi9";
-
-//Return Complete MARC Record From IdDocnum 
 
 app.MapGet("/GetRecordFromIdDocnum/{idDocnum}", async (string idDocnum) =>
 {
-    using var httpClient = new HttpClient();
-    try
-    {
-        string apiUrl = "https://sigb.philharmoniedeparis.fr/sdk/documentService.svc/FindDocuments";
-
-        var requestData = new
-        {
-            query = $"IDOCNUM856B={idDocnum}",
-            start = 0,
-            limit = 10,
-            sort = "ANPA",
-            sortAscending = true
-        };
-        var content = new StringContent(
-            System.Text.Json.JsonSerializer.Serialize(requestData),
-            System.Text.Encoding.UTF8,
-            "application/json");
-
-        httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-        httpClient.DefaultRequestHeaders.Add("accessKey", accessKey);
-        var response = await httpClient.PostAsync(apiUrl, content);
-        response.EnsureSuccessStatusCode();
-
-
-
-        var jsonResponse = await response.Content.ReadAsStringAsync();
-
-        //return Results.Content(jsonResponse, "application/json");
-
-        // Parse the JSON to extract just the XML content
-        using var jsonDoc = System.Text.Json.JsonDocument.Parse(jsonResponse);
-        var root = jsonDoc.RootElement;
-
-        if (root.TryGetProperty("d", out var d))
-        {
-            // Get the XML string from the first result
-            string xmlData = d.ToString();
-            if (string.IsNullOrEmpty(xmlData))
-            {
-                return Results.NotFound("No XML data found in the response");
-            }
-            
-            FileMARCXML marcRecord = new FileMARCXML(xmlData);
-            
-            //return Results.Content(marcRecord.RawSource "application/xml");
-
-            Record firstRecord = marcRecord[0];
-            Field idSyracuseField = firstRecord["001"];
-
-            string idSyracuse = idSyracuseField.FormatField();
-
-            // Return just the XML content
-            //return Results.Content(xmlData, "application/xml");
-        }
-
-        return Results.NotFound("No XML data found in the response");
-
-    }
-    catch (HttpRequestException ex)
-    {
-        // Handle HTTP request errors
-        return Results.Problem($"Error fetching data: {ex.Message}");
-    }
+    string xmlData = await Tools.getSyracuseRecordFromIdDocnum(idDocnum);
+    return Results.Content(xmlData, "application/xml");
 });
+
+app.MapGet("/GetRecordFromIdSyracuse/{idSyracuse}", async (string idSyracuse) =>
+{
+    string xmlData = await Tools.getSyracuseRecordFromId(idSyracuse);
+    return Results.Content(xmlData, "application/xml");
+});
+
 
 app.Run();
 
---
+++ MedUtils/Tools/Tools.cs
@@ -0,0 +1,91 @@
+﻿using MARC;
+
+namespace MedUtils.Tools
+{
+    public class Tools
+    {
+        public static class SyracuseParams
+        {
+            public const string apiUrl = "https://sigb.philharmoniedeparis.fr/sdk/documentService.svc/";
+            public const string accessKey = "85kxBW8Lm2vQi9";
+        }
+        public static async Task<string> postSyracuseRequest(object requestData, string Url)
+        {
+            using var httpClient = new HttpClient();
+
+            var requestContent = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(requestData),
+                System.Text.Encoding.UTF8,
+                "application/json");
+
+            try
+            {
+                httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+                httpClient.DefaultRequestHeaders.Add("accessKey", SyracuseParams.accessKey);
+                var response = await httpClient.PostAsync(Url, requestContent);
+                response.EnsureSuccessStatusCode();
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                return jsonResponse;
+            }
+            catch (HttpRequestException e)
+            {
+                // Handle exception
+                return e.Message;
+            }
+        }
+
+        public static string ExtractXMLFromJson(string jsonResponse)
+        {
+            using var jsonDoc = System.Text.Json.JsonDocument.Parse(jsonResponse);
+            var root = jsonDoc.RootElement;
+            if (root.TryGetProperty("d", out var d))
+            {
+                // Get the XML string from the first result
+                string xmlData = d.ToString();
+                if (string.IsNullOrEmpty(xmlData))
+                {
+                    return "No XML data found in the response";
+                }
+                return xmlData;
+            }
+            else
+            {
+                return "No 'd' property found in the response";
+            }
+        }
+
+        public static async Task<string> getSyracuseRecordFromId(string idSyracuse)
+        {
+            string xmlRecord = "";
+            string requestResponse = "";
+            string apiUrlGetDoc = SyracuseParams.apiUrl + "GetDocument";
+            //test
+
+            var requestData = new
+            {
+                listIds = new List<string> { idSyracuse }
+            };
+
+            requestResponse = await postSyracuseRequest(requestData, apiUrlGetDoc);
+            xmlRecord = ExtractXMLFromJson(requestResponse);
+            return xmlRecord;
+        }
+        public static async Task<string> getSyracuseRecordFromIdDocnum(string idDocnum)
+        {
+            string xmlRecord = "";
+            string requestResponse = "";
+            string apiUrlFindDocs = SyracuseParams.apiUrl + "FindDocuments";
+            var requestData = new
+            {
+                query = $"IDOCNUM856B={idDocnum}",
+                start = 0,
+                limit = 10,
+                sort = "ANPA",
+                sortAscending = true
+            };
+            requestResponse = await postSyracuseRequest(requestData, apiUrlFindDocs);
+            xmlRecord = ExtractXMLFromJson(requestResponse);
+            return xmlRecord;
+        }
+    }
+}
