485053278ab52e79d87ea9b62373bf7e5412f957
ddenocq <ddenocq@cite-musique.fr>
2025-05-28 17:44:49 +0200

    Validation

---
+++ MedUtils/Features/Deneb/DenebEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace MedUtils.Features.Deneb
+{
+    public static class DenebEndpoints
+    {
+        public static void MapDenebEndpoints(this WebApplication app)
+        {
+            var BasicGroup = app.MapGroup("/Deneb");
+           
+            //BasicGroup.MapGet("/event/{id}", DenebHandlers.GetEventFromId);
+            BasicGroup.MapGet("/token", DenebHandlers.GetToken);
+
+        }
+    }
+}
